9ad084411b47b5909906effe6db9a61272c70987
willlam <will@willlam.net>
2014-01-17 01:03:49 +0000

    Temp move

---
+++ app2/models/visitor.rb
@@ -0,0 +1,19 @@
+class Visitor < ActiveRecord::Base
+  has_no_table
+  column :email, :string
+  validates_presence_of :email
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+
+  def subscribe
+   mailchimp = Gibbon::API.new
+   result = mailchimp.lists.subscribe({
+    :id => ENV['MAILCHIMP_LIST_ID'],
+     :email => {:email => self.email},
+     :double_optin => false,
+     :update_existing => true,
+     :send_welcome => true
+    })
+    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result 
+    end
+
+end
